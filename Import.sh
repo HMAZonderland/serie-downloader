f8fd51fd59be6e62b180ae995e79bd3dc73ee5ed
Hugo Zonderland <hugo@webiq.nl>
2016-02-17 10:48:45 +0100

    Added params.conf => config file to separate configuration from program source Updated Import.sh to 1. use the params.conf file for paramters and 2. clear the local and remote media directories Updated README.md to instruct on filling the params.conf file based on the sample file.

--- Import.sh
+++ Import.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-### Assign variables
-HOST=""
-USERNAME=""
-RWD=""
-LCD=""
-MCD=""
+#Load config file
+source params.conf
 
 ### Collect and download files from the remote host
 echo "Connecting to $USERNAME@$HOST and downloading files from $RWD to $LCD recursively"
@@ -18,17 +14,27 @@ echo "Done downloading!"
 
 ## Unpack the collected files on disk..
 echo "Checking if there are files to be extracted/processed.."
-if test "$(ls -A "$LCD")"; then
+if test "$(ls -A $LCD)"; then
+    ## Find parted .rar files and extract them using unrar
     echo "There are files in $LCD, going to recursively extract 'em.."
-    find $LCD/ -name -type f '*.rar' -execdir unrar r {} \;
+    find $LCD/ -name '*.rar' -execdir unrar e {} \;
 
+    ## Remove any extracted sample mkv files
     echo "Removing any sample .mkv files.."
-    find $LCD/ -name -type f '*sample*.mkv' -execdir rm -f {} \;
+    find $LCD/ -name '*sample*.mkv' -execdir rm -f {} \;
 
+    ## Move the files into your media directory (manual postprocessing folder for example)
     echo "Done extracting video files, now move extracted .mkv files.."
-    find $LCD/ -name -type f '*.mkv' -execdir mv {} $MCD \;
+    find $LCD/ -name '*.mkv' -execdir mv {} $MCD \;
 
-    echo "DONE!"
+    ## Remove the incomming directory
+    echo "Lets clear the $LCD directory.. "
+    rm -rf $LCD/*
+
+    ## Clear the remote directory
+    echo "Clearing $USERNAME@$HOST/$RWD.."
+    ssh "$USERNAME@$HOST" "rm -rf $RWD/*"
+    echo "done"
 else
     echo "No files where found in $LCD, quitting this job"
 fi
--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 ###What?
-Downloads files (rared media files) using sftp from a remote host to a local directory. Extracts them using unrar (please install!) and moves them to you're media directory.
+Downloads files (rared media files) using sftp from a remote host to a local directory. Extracts them using unrar (please install!) and moves them to you're media directory. After that the local download and the remote source directory is cleared. 
 
 #####Requirements
 - sftp
+- ssh
 - ssh-key
 - unrar
 
 #####Variables
+Please set these variables by creating a file named `params.conf`. You can view `params.sample.conf` as sample. 
+
 - `HOST` - hostname of the remote host
 - `USERNAME` - username to login with
 - `RWD` - remote directory, to fetch files from
